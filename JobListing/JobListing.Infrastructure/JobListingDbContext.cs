fc2552fc449da86c1621f2ad55faefdc1ffb5b5f
emilia98 <emilia_nedialkova@abv.bg>
2023-08-16 17:06:30 +0300

    add base EF packages

---
+++ JobListing/JobListing.Infrastructure/JobListingDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobListing.Infrastructure
+{
+    public class JobListingDbContext : DbContext
+    {
+        public JobListingDbContext(DbContextOptions<JobListingDbContext> options)
+            : base(options)
+        { }
+    }
+}

c37403608edc11e9e5006d3931c04178faeaa4ed
emilia98 <emilia_nedialkova@abv.bg>
2023-08-17 19:17:15 +0300

    add Language Service

---
+++ JobListing/JobListing.Core/Contracts/ILanguageService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobListing.Core.Contracts
+{
+    public interface ILanguageService
+    {
+        // GetAllAsync
+
+        // GetByIdAsync
+
+        // AddAsync
+
+        // EditAsync
+
+        // DeleteAsync
+    }
+}
--- JobListing/JobListing.MVC/Program.cs
+++ JobListing/JobListing.MVC/Program.cs
@@ -16,11 +16,16 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<ITechnologyService, TechnologyService>();
+builder.Services.AddScoped<ILanguageService, LanguageService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+   
+} 
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
